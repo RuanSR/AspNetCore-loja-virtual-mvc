b9a032b856c61f5d1a75f651dcd73e1fba969a7b
RuanSR <ruansilvarosa@gmail.com>
2020-06-08 14:51:58 -0300

    Adaptando Formulario de dados e recebendo dados.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,6 +20,15 @@ namespace LojaVirtual.Controllers
             return View();
         }
 
+        public IActionResult ContatoAcao()
+        {
+            string nome = HttpContext.Request.Form["nome"];
+            string email = HttpContext.Request.Form["email"];
+            string texto = HttpContext.Request.Form["texto"];
+
+            return new ContentResult() { Content = string.Format("Dados recebidos com sucesso!<br/> Nome: {0} <br/>E-mail: {1} <br/>Texto: {2}", nome, email, texto), ContentType = "text/html" };
+        }
+
         public IActionResult Login()
         {
             return View();
